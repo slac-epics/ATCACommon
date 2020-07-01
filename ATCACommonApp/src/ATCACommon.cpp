ef7506d0ff457b678afaaf64ad66a35166071460
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2020-06-30 23:24:14 -0700

    Removed old commented code

--- ATCACommonApp/src/ATCACommon.cpp
+++ ATCACommonApp/src/ATCACommon.cpp
@@ -425,8 +425,7 @@ static long atcaCommonAsynDriverInitialize(void)
                       epicsThreadGetStackSize(epicsThreadStackMedium),
                       (EPICSTHREADFUNC) atcaCommonAsynDriverPollThread, (void *) NULL);
     epicsAtExit3((epicsExitFunc) stopPollThread, (void*) epicsStrDup(name), epicsStrDup(name));
-//    debugStreamAsynDriver_createStreamThreads();
-//gmdDebugStream_createStreamThreads();
+    
     return 0;
 }
 
